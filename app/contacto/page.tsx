8b8265c129b6253cb7c60bd0b240cb5759bf1e68
Rocío Lavega <92762371+estudioideamos@users.noreply.github.com>
2026-08-12 18:10:17 -0300

    Actualizar email de contacto

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -27,7 +27,7 @@ export default function ContactPage() {
           <h2>Estamos para<br /><em>responderte.</em></h2>
           <div className="contact-data contact-page-data">
             <a href="https://wa.me/5491167681777" target="_blank" rel="noreferrer"><span>WHATSAPP</span><b>+54 9 11 6768 1777</b><i className="contact-icon contact-icon-whatsapp" aria-hidden="true"><img src="/whatsapp.svg" alt="" /></i></a>
-            <a href="mailto:hola@ideamos.ar"><span>EMAIL</span><b>hola@ideamos.ar</b><i className="contact-icon contact-icon-mail" aria-hidden="true"><span /></i></a>
+            <a href="mailto:hola@ideamos.com.ar"><span>EMAIL</span><b>hola@ideamos.com.ar</b><i className="contact-icon contact-icon-mail" aria-hidden="true"><span /></i></a>
             <div><span>BASE</span><b>Buenos Aires, Argentina</b><i className="contact-icon contact-icon-pin" aria-hidden="true"><span /></i></div>
           </div>
         </div>
